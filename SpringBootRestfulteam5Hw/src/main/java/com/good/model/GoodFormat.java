1043a44e6979b86069caae8601f9d6327637b2cd
LAPTOP-BTVDJH53\中山大文豪 <sjk678vf@gmail.com>
2024-04-28 16:25:35 +0800

    嗨 你好 我這次把表修改了一下 正在做規格表CRUD的部分

---
+++ SpringBootRestfulteam5Hw/src/main/java/com/good/model/GoodFormat.java
@@ -0,0 +1,107 @@
+package com.good.model;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+@Entity
+@Table(name = "GOODFORMAT")
+@Component
+public class GoodFormat {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="FORMATID")
+	private int formatID;
+	@Column(name="GOODSID")
+	@Transient
+	private int goodsID;
+	@Column(name="GOODSIZE")
+	private String goodSize;
+	
+	@Column(name="GOODPRICE")
+	private int goodPrice;
+	
+	@Column(name="GOODIMAGEPATH")
+	private String goodImagePath;
+	
+	@Column(name="GOODSTOCK")
+	private int goodStock;
+
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "GOODSID")
+	private GoodsBean2 good;
+	//
+	public GoodFormat(int formatID, int goodsID, String goodSize, int goodPrice, String goodImagePath) {
+		this.formatID = formatID;
+		this.goodsID = goodsID;
+		this.goodSize = goodSize;
+		this.goodPrice = goodPrice;
+		this.goodImagePath = goodImagePath;
+	}
+	public GoodFormat() {
+	
+	}
+	//
+	public GoodsBean2 getGood() {
+		return good;
+	}
+	public void setGood(GoodsBean2 good) {
+		this.good = good;
+	}
+	
+	public int getFormatID() {
+		return formatID;
+	}
+
+
+	public void setFormatID(int formatID) {
+		this.formatID = formatID;
+	}
+
+	public int getGoodsID() {
+		return goodsID;
+	}
+
+	public void setGoodsID(int goodsID) {
+		this.goodsID = goodsID;
+	}
+
+	public String getGoodSize() {
+		return goodSize;
+	}
+
+	public void setGoodSize(String goodSize) {
+		this.goodSize = goodSize;
+	}
+
+	public int getGoodPrice() {
+		return goodPrice;
+	}
+
+	public void setGoodPrice(int goodPrice) {
+		this.goodPrice = goodPrice;
+	}
+
+	public String getGoodImagePath() {
+		return goodImagePath;
+	}
+
+	public void setGoodImagePath(String goodImagePath) {
+		this.goodImagePath = goodImagePath;
+	}
+	
+	
+	
+}
--- SpringBootRestfulteam5Hw/src/main/java/com/good/model/GoodRepository.java
+++ SpringBootRestfulteam5Hw/src/main/java/com/good/model/GoodRepository.java
@@ -6,15 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 
-public interface GoodRepository extends JpaRepository<GoodsBean, Integer> {
+public interface GoodRepository extends JpaRepository<GoodsBean2, Integer> {
 
-	@Query(value = "from GoodsBean where goodsName like concat('%',?1,'%1')")
-	public List<GoodsBean> findGoods(String goodsName);
+	@Query(value = "from GoodsBean2 where goodsName like concat('%',?1,'%1')")
+	public List<GoodsBean2> findGoods(String goodsName);
 
 //	public List<GoodsBean> findByNameLike(String goodsName);
 	
 	@Query(value = "select * from Goods", nativeQuery = true)//用標準SQL語法
-	public List<GoodsBean> findAll();
+	public List<GoodsBean2> findAll();
 	
 
 }

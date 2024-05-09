6f40593f536e1406791d07c3ea6dd844446a9ae9
sean <x0988176677@gmail.com>
2024-05-09 16:15:02 +0800

    6

--- SpringBootRestfulteam5Hw/src/main/webapp/WEB-INF/Order/js/datatables-simple-demo.js
+++ SpringBootRestfulteam5Hw/src/main/webapp/WEB-INF/Order/js/datatables-simple-demo.js
@@ -7,9 +7,9 @@ window.addEventListener('DOMContentLoaded', event => {
     searchTitle: "Search within table",
     pageTitle: "Page {page}",
     perPage: "筆1頁",
-    noRows: "查無資料",
+    noRows: "目前沒有資料",
     info: "第 {start} 到第 {end} 筆訂單 總共 {rows} 筆訂單",
-    noResults: "No results match your search query",
+    noResults: "查無資料",
 }
         });
         
